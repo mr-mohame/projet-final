35a83143ffe9538b66656ae9bcdd32ee7c2c3d03
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-21 11:20:35 +0000

    ajouter le menu dans le switch

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const apprenants = [
 },
 {
     id: 4,
-    nomComplet: "Amine js",
+    nomComplet: "Amine Js",
     ville: "fes",
     resultats: [
         { jour: 1, exercicesTermines: 10, 
@@ -255,19 +255,19 @@ function  trierParAlphabetique(tableau){
     return tableau;
 }
 
-let choix;
-console.log("=== SAS PROGRESS CONSOLE ===");
-console.log("1. Afficher le tableau de bord");
-console.log("2. Afficher la liste des apprenants");
-console.log("3. Ajouter un apprenant");
-console.log("4. Consulter un apprenant par identifiant");
-console.log("5. Ajouter ou modifier le résultat d'une journée");
-console.log("6. Rechercher un apprenant par nom");
-console.log("7. Filtrer les apprenants par niveau");
-console.log("8. Trier les apprenants par progression décroissante");
-console.log("9. Trier les apprenants par ordre alphabétique");
-console.log("0. Quitter");   
+let choix;  
 do{
+    console.log("=== SAS PROGRESS CONSOLE ===");
+    console.log("1. Afficher le tableau de bord");
+    console.log("2. Afficher la liste des apprenants");
+    console.log("3. Ajouter un apprenant");
+    console.log("4. Consulter un apprenant par identifiant");
+    console.log("5. Ajouter ou modifier le résultat d'une journée");
+    console.log("6. Rechercher un apprenant par nom");
+    console.log("7. Filtrer les apprenants par niveau");
+    console.log("8. Trier les apprenants par progression décroissante");
+    console.log("9. Trier les apprenants par ordre alphabétique");
+    console.log("0. Quitter");  
     choix = Number(prompt("entre le choix :"));
     switch(choix){
         case 1:
